7cc6c81e22a813d22787316d151f01cf0346130e
giamazonas <skibinskirenee@gmail.com>
2022-03-14 16:29:49 -0400

    repo correct

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import FloorPlan from './FloorPlan';
 
 function App() {
+  const floorPlan = [
+    {name: 'LivingRoom', },
+    {name: 'Bedroom Master', bedNum: 1 },
+    {name: 'Bedroom 1', bedNum: [1,2,3,] },
+    {name: 'Bedroom 2', bedNum: 3},
+    {name: 'Bath', size: ['half', 'full' ] },
+    {name: 'Kitchen', contains: ['Oven', 'Sink'] },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="FloorPlan">
+      <FloorPlan floorplan={floorPlan}/>
     </div>
   );
 }
